00cebd5f18653a4412534a83023d5e68d40885a3
Chris Weyl <cweyl@alumni.drew.edu>
2013-10-01 18:00:31 -0700

    initial commit

---
+++ lib/autobox/Junctions.pm
@@ -0,0 +1,19 @@
+package autobox::Junctions;
+
+# ABSTRACT: The great new autobox::Junctions!
+
+use Moose;
+use namespace::autoclean;
+
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
