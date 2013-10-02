15b746ff4bbcc77faa89251e54c0484269a1e1b4
Chris Weyl <cweyl@alumni.drew.edu>
2013-10-01 18:28:32 -0700

    Initial implementation and tests

--- lib/autobox/Junctions.pm
+++ lib/autobox/Junctions.pm
@@ -1,19 +1,85 @@
 package autobox::Junctions;
 
-# ABSTRACT: The great new autobox::Junctions!
+# ABSTRACT: Autoboxified junction-style operators
 
-use Moose;
-use namespace::autoclean;
+use strict;
+use warnings;
 
+use parent 'autobox';
 
-__PACKAGE__->meta->make_immutable;
+sub import {
+    my $class = shift @_;
+
+    $class->SUPER::import(
+        ARRAY => 'autobox::Junctions::ARRAY',
+        @_,
+    );
+}
+
+{
+    package autobox::Junctions::ARRAY;
+
+    use strict;
+    use warnings;
+
+    use Syntax::Keyword::Junction ();
+
+    sub all  { Syntax::Keyword::Junction::all( @{ $_[0] }) }
+    sub any  { Syntax::Keyword::Junction::any( @{ $_[0] }) }
+    sub none { Syntax::Keyword::Junction::none(@{ $_[0] }) }
+    sub one  { Syntax::Keyword::Junction::one( @{ $_[0] }) }
+}
+
 !!42;
 __END__
 
+=for :stopwords autoboxified autoboxifying AUTOBOXED
+
 =head1 SYNOPSIS
+
+    # somewhere above...
+    use autobox::Junctions;
+
+    # somewhere below...
+    my @explodey = qw{ bing bang boom };
+    warn "boom!\n"
+        if @explody->any eq 'boom';
+
+    my $still_explody = [ @explodey ];
+    warn "not all explody\n"
+        unless $still_explody->all eq 'boom';
+
+    # now, bonus points...
+    use autobox::Core;
+
+    my $weapons = {
+        mateu => 'bow & arrow',     # fearsome hunter
+        ether => 'disarming smile', # Canadian
+        jnap  => 'shotgun',         # upstate NY
+    };
+
+    warn 'mateu is armed!'
+        if $weapons->keys->any eq 'mateu'
+
+    warn '...but at least no one has a nuke'
+        if $weapons->values->none eq 'nuke';
 
 =head1 DESCRIPTION
 
+This is a simple autoboxifying wrapper around L<Syntax::Keyword::Junction>,
+that provides array and array references with the functions provided by that
+package as methods for arrays:
+L<any|Syntax::Keyword::Junction/any>,
+L<all|Syntax::Keyword::Junction/all>, L<one|Syntax::Keyword::Junction/one>,
+and L<none|Syntax::Keyword::Junction/none>.
+
+=head1 AUTOBOXED METHODS
+
+See: L<Syntax::Keyword::Junction/any>, LSyntax::Keyword::Junction/all>,
+L<Syntax::Keyword::Junction/one>, and L<Syntax::Keyword::Junction/none>.
+
 =head1 SEE ALSO
+
+Syntax::Keyword::Junction
 
 =cut
--- t/basic.t
+++ t/basic.t
@@ -2,7 +2,33 @@ use strict;
 use warnings;
 
 use Test::More;
+use autobox::Junctions;
 
-fail 'No tests!';
+# we really only need to test the autoboxification sticks; the junctions
+# themselves have (presumably) been tested in their own test suites.
+
+my @one = ( 'a', 1, 2 );
+
+subtest 'check with @array' => sub {
+
+    ok @one->any   eq 2, 'any OK';
+    ok @one->none  eq 'ten', 'none OK';
+    ok !(@one->all eq 1), 'all OK';
+    ok @one->one   eq 1, 'one OK';
+
+    return;
+};
+
+my $two = [ @one ];
+
+subtest 'check with @array' => sub {
+
+    ok $two->any   eq 2, 'any OK';
+    ok $two->none  eq 'ten', 'none OK';
+    ok !($two->all eq 1), 'all OK';
+    ok $two->one   eq 1, 'one OK';
+
+    return;
+};
 
 done_testing;
